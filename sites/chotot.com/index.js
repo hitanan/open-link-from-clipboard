e54ed9c176d837dec97003cbcbaf9b5e9016b4ee
An Nguyen <hitanan@gmail.com>
2016-08-10 15:55:21 +0700

    init2

--- sites/chotot.com/index.js
+++
@@ -1,8 +0,0 @@
-//alert("1");
-
-jQuery(".listing-rows .chotot-list-row").each(function(){
-	console.log(jQuery(this));
-	if (jQuery(this).has('span.ad-type-suf').length ) {
-		jQuery(this).remove();
-	}
-});
